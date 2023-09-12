97ff80633258bedc04c67fe25c7c0f111d82c706
LargeElmo <104458107+LargeElmo@users.noreply.github.com>
2023-09-11 20:00:17 -0400

    Create basic_01.py
    Continuing examining the variations  in creation methods for repositories.

---
+++ basic_01.py
@@ -0,0 +1,6 @@
+# https://youtu.be/iv8rSLsi1xo?t=353
+
+print("This file is the first one in a repository that was created on GitHub and then cloned to this local machine.")
+
+print("First training with this particular method.")
+
